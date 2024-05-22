b5501253d22dcb7920158a34bac06a6a9abbff83
Vicknesh Balasubramaniam <roninprogrammer@gmail.com>
2024-05-22 23:53:40 +0800

    change to getMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'AFC Official Merchandise',
       theme: ThemeData(
         // This is the theme of your application.
