320095b5ee5dbef74a172ee31a1240d0ed8bf1b9
Vicknesh Balasubramaniam <roninprogrammer@gmail.com>
2024-05-29 13:45:47 +0800

    add database and cloud

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ exports.makeDb = () => {
   mongoose.set("useCreateIndex", true);
   mongoose.connect(
     // Update here mongodb access credential
-    'mongodb+srv: ... ',
+    'mongodb://afcassignment:Afc123456@ac-9bcfjiu-shard-00-00.gnjkhdu.mongodb.net:27017,ac-9bcfjiu-shard-00-01.gnjkhdu.mongodb.net:27017,ac-9bcfjiu-shard-00-02.gnjkhdu.mongodb.net:27017/?ssl=true&replicaSet=atlas-6cqsq2-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0',
     {
       useNewUrlParser: true,
       useCreateIndex: true,
